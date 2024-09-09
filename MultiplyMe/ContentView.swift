9273cdc423139c36f4fc388cce2a15345682ca4a
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-09 17:13:47 +0300

    Basic UI

--- MultiplyMe/ContentView.swift
+++ MultiplyMe/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedMultiplicationTable = 2
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                LinearGradient(colors: [Color(red: 255 / 255, green: 190 / 255, blue: 176 / 255), Color(red: 255 / 255, green: 110 / 255, blue: 106 / 255)], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack {
+                    Stepper("Which number do you want to practice?", value: $selectedMultiplicationTable, in: 2...12, step: 1)
+                        .padding()
+                        .background(.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    HStack {
+                        Text("You have selected:")
+                            .font(.headline)
+                        Text("\(selectedMultiplicationTable)")
+                            .font(.headline.bold())
+                    }
+                    .padding()
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
