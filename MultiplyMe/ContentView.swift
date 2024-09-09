dee4d978476d57417baf887e050d9163b8acbca8
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-09 17:38:11 +0300

    Some usability added

--- MultiplyMe/ContentView.swift
+++ MultiplyMe/ContentView.swift
@@ -7,8 +7,31 @@
 
 import SwiftUI
 
+struct RoundedRectangleStyle: ViewModifier {
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+        .padding()
+        .background(color)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+extension View {
+    func roundedRectangleStyle(color: Color) -> some View {
+        modifier(RoundedRectangleStyle(color: color))
+    }
+}
+
 struct ContentView: View {
+    let questionAmounts = ["5", "10", "20"]
+    
     @State private var selectedMultiplicationTable = 2
+    @State private var selectedQuestionAmount = "5"
+    
+    @State private var questions = [String]()
+    @State private var rightAnswers = [Int]()
     
     var body: some View {
         NavigationStack {
@@ -16,25 +39,45 @@ struct ContentView: View {
                 LinearGradient(colors: [Color(red: 255 / 255, green: 190 / 255, blue: 176 / 255), Color(red: 255 / 255, green: 110 / 255, blue: 106 / 255)], startPoint: .top, endPoint: .bottom)
                     .ignoresSafeArea()
                 VStack {
-                    Stepper("Which number do you want to practice?", value: $selectedMultiplicationTable, in: 2...12, step: 1)
-                        .padding()
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                    
-                    HStack {
-                        Text("You have selected:")
-                            .font(.headline)
-                        Text("\(selectedMultiplicationTable)")
-                            .font(.headline.bold())
+                    VStack(alignment: .leading) {
+                        Stepper(value: $selectedMultiplicationTable, in: 2...12, step: 1) {
+                            HStack {
+                                Text("Which number?")
+                                Spacer()
+                                Text("\(selectedMultiplicationTable)")
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        Text("How many questions?")
+                            .padding(.top)
+                        Picker("How many questions?", selection: $selectedQuestionAmount) {
+                            ForEach(questionAmounts, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        .pickerStyle(.segmented)
                     }
-                    .padding()
-                    .background(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                
+                    .roundedRectangleStyle(color: .white)
+                    
+                    Button("Start new game") {
+                        
+                    }
+                    .roundedRectangleStyle(color: .white)
+                    
+                    Spacer()
+                        
                 }
                 .padding()
             }
         }
+    }
+    
+    func startGame() {
+        
+    }
+    
+    func generateQuestions() {
+        
     }
 }
 
