0e54952df8c861b33734c00d9d936c168d4b70a8
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-09 16:25:48 +0300

    Initial Commit

---
+++ MultiplyMe/MultiplyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyMeApp.swift
+//  MultiplyMe
+//
+//  Created by Mykola Chaikovskyi on 09.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
